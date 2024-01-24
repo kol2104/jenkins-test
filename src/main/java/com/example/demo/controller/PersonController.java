87dc3e15f5c0a490ee58196f9d389ea5067a1fe0
nikolai_karasev <kni2104kni@mail.ru>
2024-01-25 02:16:16 +0500

    Updated path

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/person")
 public class PersonController {
 
     private final PersonService personService;
