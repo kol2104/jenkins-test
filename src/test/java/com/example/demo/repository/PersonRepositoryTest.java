a5d0337883e276f54c3c068a5f553db45af5c339
nikolai_karasev <kni2104kni@mail.ru>
2024-01-29 00:12:41 +0500

    Added one test

--- src/test/java/com/example/demo/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/demo/repository/PersonRepositoryTest.java
@@ -1,13 +1,37 @@
 package com.example.demo.repository;
 
-import com.example.demo.DemoApplicationTests;
+import com.example.demo.model.Person;
+import com.example.demo.service.impl.PersonServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
 public class PersonRepositoryTest {
 
-    @Test
-    public void contextLoad() {
+    @Mock
+    private PersonRepository personRepository;
+    @InjectMocks
+    private PersonServiceImpl personServiceImpl;
 
+    @Test
+    public void handleGetPersons_ReturnValidList() {
+        List<Person> list = List.of(
+                new Person(1L, "name"),
+                new Person(2L, "name2")
+        );
+        doReturn(list).when(personRepository).findAll();
+
+        List<Person> resultList = personServiceImpl.getPersons();
+
+        assertEquals(list.size(), resultList.size());
+        assertEquals(list, resultList);
     }
 }
