edbbec06a1b5afd442ebaea8965af144b6d52ae4
nikolai_karasev <kni2104kni@mail.ru>
2024-01-29 23:52:18 +0500

    Fixed bugs

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,14 +1,11 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 
 @Data
 @AllArgsConstructor
@@ -17,15 +14,6 @@ import org.hibernate.annotations.Parameter;
 @Table(name = "persons")
 public class Person {
     @Id
-    @GeneratedValue(generator = "sequence-generator")
-    @GenericGenerator(
-            name = "sequence-generator",
-            parameters = {
-                    @Parameter(name = "sequence_name", value = "user_sequence"),
-                    @Parameter(name = "initial_value", value = "4"),
-                    @Parameter(name = "increment_size", value = "1")
-            }
-    )
     private Long id;
     private String name;
 }
